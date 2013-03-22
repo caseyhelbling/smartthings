e8764564ed4e5a7c3c177af60214b387e5269ba0
Casey Helbling <casey@softwareforgood.com>
2013-03-21 21:09:05 -0500

    more of the first spec

---
+++ spec/api_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe "SmartThings::API" do
+
+  describe "server" do
+    subject { SmartThings::API.new('12345').server(opts) }
+
+    context "for ssl" do
+      let(:opts) { { :use_ssl => true } }
+      it "should be https" do
+        subject.should == 'https://graph.api.smartthings.com'
+      end
+    end
+    
+    context "without ssl" do
+      let(:opts) { { } }
+      it "should be http" do
+        subject.should == 'http://graph.api.smartthings.com'
+      end
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+require 'faraday'
+require 'multi_json'
+require 'smart_things'
+
+#Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  #config.use_transactional_fixtures = false
+end
+
